fc8b632c03d6d90cfa7948c434ca152f38786b40
wbh1328551759 <1328551759@qq.com>
2023-04-21 18:09:43 +0800

    feat: generate third-web3-demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WalletProvider } from '@suiet/wallet-kit';
+import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
+import {configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [goerli],
+  [publicProvider()],
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+
+      <ThirdwebProvider activeChain="goerli">
+
+          <WalletProvider chains={[
+            {
+              id: 'sui:testnet',
+              name: 'Sui Testnet',
+              rpcUrl: 'https://sui-testnet-wave3.coming.chat',
+            }
+          ]}>
+            <Component {...pageProps} />
+          </WalletProvider>
+        </ThirdwebProvider>
+    </WagmiConfig>
+
+)
 }
