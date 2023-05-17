c2a71e7740e4d6b5b853786c3f6b873e4519debc
wbh1328551759 <1328551759@qq.com>
2023-05-17 18:26:28 +0800

    feat: add getBalance

--- pages/starknet.tsx
+++ pages/starknet.tsx
@@ -1,8 +1,10 @@
-import {useAccount, useConnectors, useNetwork, useWaitForTransaction } from '@starknet-react/core'
+import {useAccount, useConnectors, useContractRead, useNetwork, useWaitForTransaction } from '@starknet-react/core'
 import { Call, RawArgs, stark, uint256 } from 'starknet'
 import BigNumber from 'bignumber.js'
 import {ethers} from 'ethers'
 import {useState} from 'react'
+import ERC20ABI from '../constants/starknet_erc20.json'
+import { utils } from 'ethers'
 
 const Starknet = () => {
   const { address, account } = useAccount()
@@ -12,6 +14,12 @@ const Starknet = () => {
   const { data, error, isLoading } = useWaitForTransaction({
     hash,
     watch: true,
+  })
+  const {data: balance, isLoading: isLoadingBalance} = useContractRead({
+    address: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+    abi: ERC20ABI,
+    functionName: 'balanceOf',
+    args: [address]
   })
   // console.log('account',chain, connector)
 
@@ -119,7 +127,31 @@ const Starknet = () => {
         '0'
       ]
     }
-    const response = await account?.execute([approveCall, swapCall])
+    const testData = [
+      {
+        "contractAddress": "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+        "entrypoint": "approve",
+        "calldata": [
+          "0x01b01a0b51c67fa18d821dad0af4df0fce1bd41a7340e497bb41745a5c08295c",
+          "3838173627289100",
+          "0"
+        ]
+      },
+      {
+        "contractAddress": "0x01b01a0b51c67fa18d821dad0af4df0fce1bd41a7340e497bb41745a5c08295c",
+        "entrypoint": "swap_out",
+        "calldata": [
+          "0x00f288712fc0c7cb19fa388550ace525000000006464a01903e902dcc98ac53a",
+          "5",
+          "0x1aA5eF6233f209927861B61085074767CE94c18C",
+          "3838173627289100",
+          "0",
+          "2838173627289000",
+          "0"
+        ]
+      }
+    ]
+    const response = await account?.execute(testData)
     setHash(response?.transaction_hash ?? '')
     console.log('response', response?.transaction_hash)
 
@@ -130,6 +162,7 @@ const Starknet = () => {
       {address ? (
         <div>
           当前账户：{address}
+          <div>Balance: {isLoadingBalance && 'loading...'} {utils.formatUnits(balance?.[0]?.low.toString(), 18)}</div>
           <button onClick={disconnect}>disconnect</button>
         </div>
       ) : (
